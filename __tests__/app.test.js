e87ff15d7c194a879f5d44c4cc302671c51e716b
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-04 22:14:50 -0700

    Finished all the extra requirements

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,11 @@ import {
   evensOnlyAndDoubleArray,
   findItem,
   sortArray,
+  doubleLikes,
+  moreThan100Likes,
+  numberOfLikes,
+  findObjByName,
+  sortArtistByName
 } from '../app'
 
 //hint look into before_each before_all
@@ -77,3 +82,72 @@ describe('originalArray Tests', () => {
     })
   
   }) 
+
+
+
+
+
+describe('doubleLikes Tests', () => {
+  test('Given an object array, double likes', ()=>{
+
+    let artists = [
+      {name: 'a', likes:100},
+      {name: 'c', likes:99},
+      {name: 'c', likes:101}, ]
+  
+    expect(doubleLikes(artists)).toEqual([ {name: 'a', likes:200},
+    {name: 'c', likes:198},
+    {name: 'c', likes:202},
+   ])
+ })
+}) 
+
+describe('moreThan100Likes Tests', () => {
+  let artists = [
+    {name: 'a', likes:100},
+    {name: 'c', likes:99},
+    {name: 'c', likes:101}, ]
+
+  test('Given an object array, return artist with over 100 likes', ()=>{
+    
+    expect(moreThan100Likes(artists)).toEqual([{name: 'c', likes:101}])
+  })
+  test('Given a number to find in array, if number not in an array, return string "Not Found"', ()=>{
+    expect(findItem([2,2,7,8])).toEqual('Not Found')
+  })
+}) 
+let artists = [
+  {name: 'a', likes:100},
+  {name: 'c', likes:99},
+  {name: 'c', likes:101}, ]
+
+describe('numberOfLikes Tests', () => {
+
+  test('Given an object array, return total number of likes', ()=>{
+    
+    expect(numberOfLikes(artists)).toEqual(300)
+  })
+}) 
+
+
+
+describe('findObjByName Tests', () => {
+
+  test('Given an object array, find obj by input name', ()=>{
+    
+    expect(findObjByName(artists,"a")).toEqual([{name: 'a', likes:100}])
+  })
+}) 
+
+describe('sortArtistByName Tests', () => {
+
+  test('Given an object array, sort object by number of likes', ()=>{
+    
+    expect(sortArtistByName(artists)).toEqual([
+      {name: 'c', likes:99},
+      {name: 'a', likes:100},
+      {name: 'c', likes:101}, ]
+    )
+  })
+}) 
+
--- app.js
+++ app.js
@@ -85,6 +85,7 @@ export const sortArray = (numberArray) => {
   return numberArraySorted.reverse()
 }
 
+
 //=> [4,3,2,1] *desc*
 
 // artists = [
@@ -93,19 +94,70 @@ export const sortArray = (numberArray) => {
 //   {name: 'c', likes:101}, 
 // ]
 // // ARRAY ITERATOR METHODS ON Objects with name age 
+
 // // doubles likes of artist
-// doubleLikes(artists)
+export const doubleLikes = (artists) => { 
+    return artists.map((artist)=>{
+      return {name: artist.name, likes: artist.likes *2}
+    })
+  }
+//   let doubleLike = Object.fromEntries(
+//     Object.entries(artists).map(([key, value]) => [key, value*2])
+//   );
+//   return doubleLike
+//   }
+ 
+
 // // => [
 // //   {name: 'a', likes:200},
 // //   {name: 'c', likes:198},
 // //   {name: 'c', likes:202},  
 // // ]; 
-// moreThan100Likes(artists)// =>[ {name: 'c', likes:101} ]; // use filter
-// //  return an array of strings of artist name
-// justArtistMoreThan100Likes(artists) //=> ['c']; // use filter
-// // get total number of likes fo all artists
-// numberOfLikes(artists) //=> 300; // get all likes
-// // find an item method. Returns 'not found' if not found
-// findObjByName(artists, 'a') // => {name: 'a', likes:200}
-// // returns array of artist sorted by likes
-// sortArtistByName(artists)
+
+export const moreThan100Likes = (artists) => { 
+  return artists.filter((artist)=>{
+    return artist.name, artist.likes > 100})
+  }
+  
+  // =>[ {name: 'c', likes:101} ]; // use filter
+//  return an array of strings of artist name
+
+
+export const justArtistMoreThan100Likes = (artists) => { 
+  artistMoreThan100 = artists.filter((artist)=>{
+    return artist.name, artist.likes > 100})
+  return artistMoreThan100.map((artist) => {
+    return artist.name})
+  }
+
+// get total number of likes fo all artists
+export const numberOfLikes = (artists) => {
+    let artistMap = artists.map((artist) => {
+      return artist.likes})
+    return artistMap.reduce(function(accumulator,likes){
+      return accumulator + likes
+    }, 0)
+    }
+//=> 300; // get all likes
+
+// find an item method. Returns 'not found' if not found
+export const findObjByName = (artists, name) => { 
+  return artists.filter((artist)=>{
+    if (artist.name == name)
+      return artist.name, artist.likes})
+  }
+   // => {name: 'a', likes:200}
+// returns array of artist sorted by likes
+export const sortArtistByName = (artists) =>{
+  let sortedArtistActuallyByLikes = artists.sort(compare)
+  return sortedArtistActuallyByLikes
+  function compare(a,b){
+    if (a.likes < b.likes){
+      return -1;
+    }
+    if (a.likes > b.likes){
+      return 1;
+    }
+    return 0;
+  }
+}
